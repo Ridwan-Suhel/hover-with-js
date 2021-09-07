8080691aee12ead04d2238acc88509d34ca91260
Ridwan Suhel <65410320+Ridwan-Suhel@users.noreply.github.com>
2021-09-08 04:13:39 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let circle = document.querySelector(".circle");
+
+circle.addEventListener("mouseover", () => {
+    if(!circle.classList.contains("hover")){
+        circle.classList.add("hover");
+    }
+});
+circle.addEventListener("mouseleave", () => {
+    if(circle.classList.contains("hover")){
+        circle.classList.remove("hover");
+    }
+});
+
+
